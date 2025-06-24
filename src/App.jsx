0be99afa102c2be114b2c5cf3df5cb636f806658
Felipe <lipejdc@gmail.com>
2025-06-24 16:43:46 +0200

    add color state to app component, create form for ColorForm component, handle form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { initialColors } from "./lib/colors";
+import { useState } from "react";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm/ColorForm";
 import "./App.css";
+import { uid } from "uid";
 
 function App() {
+
+  const [colors, setColors] = useState(initialColors);
+  const defaultFormValues = {
+    role: "some color",
+    hex: "#123456",
+    contrastText: "#ffffff",
+  };
+
+  const handleAddColor = (colorToAdd) => {
+    setColors((prevColors) => {
+      const newColorWithId = {id: uid(), ...colorToAdd};
+      const updatedColors = [newColorWithId, ...prevColors];
+      return updatedColors;
+    })
+  }
+
   return (
     <>
-      <h1>Theme Creator</h1>
+      <ColorForm onAddColor={handleAddColor} initialFormData={defaultFormValues}/>
 
-      {initialColors.map((color) => {
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
