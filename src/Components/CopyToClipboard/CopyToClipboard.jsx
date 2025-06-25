edf896bc2b863590f00aa3cf11757f6f42794b4f
Felipe <lipejdc@gmail.com>
2025-06-25 10:43:11 +0200

    create CopyToClipboard component and add copy button with styling

---
+++ src/Components/CopyToClipboard/CopyToClipboard.jsx
@@ -0,0 +1,10 @@
+import Button from "../Button/Button"
+import "./CopyToClipboard.css";
+
+export default function CopyToClipboard() {
+    return(
+        <>
+        <Button variant="copy">Copy</Button>
+        </>
+    )
+}
